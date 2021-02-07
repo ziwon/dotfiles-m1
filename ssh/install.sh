d8d426b39b676d358cf9f47338d7d9e86d055450
Youngpil Yoon <yngpil.yoon@gmail.com>
2021-02-07 13:22:59 +0900

    Update ssh

--- ssh/install.sh
+++ ssh/install.sh
@@ -7,13 +7,13 @@ EOF
 
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-echo "Descript SSH Key.."
+echo "Decrypting SSH Key.."
 mkdir -p $HOME/.ssh
-keybase decrypt -i id_rsa.github.asc -o $HOME/.ssh/id_rsa.github
-chmod 400 $HOME/.ssh/id_rsa.github
+keybase decrypt -i id_rsa.github.asc -o id_rsa.github && chmod 400 id_rsa.github
 
 echo "Copying SSH Config.."
 link config $HOME/.ssh/config
+link id_rsa.github $HOME/.ssh/id_rsa.github
 
 cd -
 
