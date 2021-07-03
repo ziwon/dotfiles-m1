4f304d2d18e7f996afa20205350c068409c218d3
Youngpil Yoon <yngpil.yoon@gmail.com>
2021-07-03 22:44:23 +0900

    add rpi ssh private key

--- ssh/install.sh
+++ ssh/install.sh
@@ -10,10 +10,11 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 echo "Decrypting SSH Key.."
 mkdir -p $HOME/.ssh
 keybase decrypt -i id_rsa.github.asc -o id_rsa.github && chmod 400 id_rsa.github
+keybase decrypt -i id_rsa.pi.asc -o id_rsa.pi && chmod 400 id_rsa.pi
 
 echo "Copying SSH Config.."
 link config $HOME/.ssh/config
 link id_rsa.github $HOME/.ssh/id_rsa.github
+link id_rsa.pi $HOME/.ssh/id_rsa.pi
 
-cd -
-
+cd - 2>&1 1>/dev/null
