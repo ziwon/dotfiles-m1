0749af3ab85828ef65455e788c9826332c0607f4
Youngpil Yoon <yngpil.yoon@gmail.com>
2022-12-11 11:08:25 +0900

    fix: update GPG key id

--- gpg/install.sh
+++ gpg/install.sh
@@ -9,18 +9,18 @@ EOF
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 # See lastpass for your passphrase
+# rm -rf $HOME/.share/gnupg
 mkdir -p $HOME/.share/gnupg
+
+# Set permissions
+chown -R $(whoami) $HOME/.share/gnupg/
+chmod 700 $HOME/.share/gnupg
 
 # Import keybase
 curl https://keybase.io/ziwon/pgp_keys.asc | gpg --import
 
 # Import secret keys
-keybase pgp export -q 81A82D6E1ABC9F7A --secret | gpg --allow-secret-key-import --import
-
-# Set permissions
-chown -R $(whoami) $HOME/.share/gnupg/
-chmod 600 $HOME/.share/gnupg/*
-chmod 700 $HOME/.share/gnupg
+keybase pgp export -q B71B7B232316168A --secret | gpg --allow-secret-key --import
 
 cp -f ./gpg-agent.conf $HOME/.share/gnupg
 
