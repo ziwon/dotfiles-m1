2b211719d366f9dde48d0c71473cdb1d6a4222ee
Youngpil Yoon <yngpil.yoon@gmail.com>
2022-12-25 11:08:58 +0900

    fix: add conda with arm arch

--- conda/install.sh
+++ conda/install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-cat <<EOF
-conda
+cat <<'EOF'
+ a88888b.                         dP
+d8'   `88                         88
+88        .d8888b. 88d888b. .d888b88 .d8888b.
+88        88'  `88 88'  `88 88'  `88 88'  `88
+Y8.   .88 88.  .88 88    88 88.  .88 88.  .88
+ Y88888P' `88888P' dP    dP `88888P8 `88888P8
 EOF
 
 # Python 3.8 (recommended). Python 3.7 through 3.10 are supported and tested.
@@ -9,8 +14,10 @@ EOF
 : ${TORCH_GPU_ENV:=torch-gpu}
 : ${MY_CONDA_HOME:=$HOME/Workspace/miniconda}
 
+echo "PY_VERSION: $PY_VERSION"
+
 rm -rf $MY_CONDA_HOME >/dev/null 2>&1
-curl -sLf -o $HOME/Workspace/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+curl -sLf -o $HOME/Workspace/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
 bash $HOME/Workspace/miniconda.sh -b -p $MY_CONDA_HOME
 
 export PATH=$PATH:$HOME/Workspace/miniconda/bin
@@ -18,11 +25,11 @@ conda create -n $TORCH_GPU_ENV python=$PY_VERSION -y
 conda activate $TORCH_GPU_ENV || {
 	conda init zsh
   conda init bash
-	conda install -p $MY_CONDA_HOME/envs/$TORCH_GPU_ENV pytorch torchvision torchaudio -c pytorch-nightly -y
+	conda install -p $MY_CONDA_HOME/envs/$TORCH_GPU_ENV pytorch torchvision torchaudio -c pytorch -c conda-forge -y
 	conda install -p $MY_CONDA_HOME/envs/$TORCH_GPU_ENV -c conda-forge jupyter jupyterlab -y
 }
 
-echo "Conda installed.. \nPlease, start a new shell and run the following script."
+echo "Conda installed. Please, start a new shell and run the following scripts."
 echo "> conda activate $TORCH_GPU_ENV"
 echo "> python3 -c \"import torch; print(torch.backends.mps.is_available()); print(torch.backends.mps.is_built())\""
 
